836b8d326a593c23387b083d133d388eb6269635
Anice <anice@syneforge.com>
2023-07-10 12:08:43 +0300

    check new auth token

--- src/Common/Services/ActionFactory.php
+++ src/Common/Services/ActionFactory.php
@@ -10,7 +10,7 @@ class ActionFactory implements ActionFactoryInterface
     private array $controllers = [];
 
     /* Может лучше передавать перечень конкретные типов интерфейсов SetActionInterface|GetActionInterface
-    Не делать пустой обобщенный интерфейс */
+    Не делать пустой обобщенный интерфейс!!! */
     public function addAction(string $controller, string $action, ActionInterface $service): void
     {
         if (array_key_exists($controller, $this->controllers)
